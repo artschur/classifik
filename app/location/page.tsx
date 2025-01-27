af540f664650e7a3a0df4a01d9c069243f9a5e78
Arthur Schurhaus <arthurschur@icloud.com>
2025-01-27 17:09:03 -0300

    change location selection page

--- app/location/page.tsx
+++ app/location/page.tsx
@@ -4,9 +4,11 @@ import { getAvailableCities } from "@/db/queries";
 export default async function LocationsPage() {
     const cities = await getAvailableCities();
     return (
-        <div className="container mx-auto px-4 py-8">
-            <h1 className="text-3xl font-bold mb-6">Our Locations</h1>
-            <SelectCidadesCadastradas cities={cities} />
+        <div className="flex flex-col justify-center items-center px-4 py-8 w-full h-full">
+            <div className="flex flex-col items-left justify-center gap-x-4 min-h-[80vh]">
+                <h1 className="text-3xl flex font-bold mb-6 px-2">Our Locations</h1>
+                <SelectCidadesCadastradas cities={cities} />
+            </div>
         </div>
     );
 }
