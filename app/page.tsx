1740d9396f09fe29206633c52b40b04722b24ce0
Arthur Schurhaus <arthurschur@icloud.com>
2025-02-14 00:05:44 -0300

    removed unused funct

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-import { getMockImage } from '@/db/queries/images';
-
 export default async function CompanionsPage() {
-  const images = await getMockImage();
-  if (!images) return null;
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-6">Our Companions</h1>
-      {images.publicUrl}
+      <h1 className="text-3xl font-bold mb-6">Welcome to onesugar</h1>
     </div>
   );
 }
