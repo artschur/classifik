50b773a60d36f48e9592350e3d64a1d3d0a489b9
Arthur Schurhaus <arthurschur@icloud.com>
2025-04-06 09:41:10 -0300

    added redis

--- db/index.ts
+++ db/index.ts
@@ -1,6 +1,12 @@
 import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
+import { Redis } from '@upstash/redis';
+
+export const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
 
 config({ path: '.env' }); // or .env.local
 
