3349587844d4a0d954dc2cf6c2dca667b808955a
Arthur Schurhaus <arthurschur@icloud.com>
2025-05-18 13:28:02 -0300

    changed prices again

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,60 +1,62 @@
-import { hasCompanionPaid } from "@/db/queries/companions";
-import { auth } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
-import { ProductCard } from "./productCard";
+import { hasCompanionPaid } from '@/db/queries/companions';
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+import { ProductCard } from './productCard';
 
 export interface Product {
-    id: string;
-    name: string;
-    description: string;
-    price: string;
+  id: string;
+  name: string;
+  description: string;
+  price: string;
 }
 const products = [
-    {
-        // id: "price_1RAt1OCuEJW1dWBav2YzW54D",
-        // id: "price_1RIsByEJQRIZgEwenkVNKNPt",
-        id: "price_1RQ8O9EJQRIZgEweScjlRZY1",
-        name: "7 Dias De Anúncio",
-        description: "Seu perfil vai ser visivel no topo da sua cidade por 7 dias",
-        price: "€52.00"
-    },
-    {
-        // id: "price_1RAt2nCuEJW1dWBajoTLZQF1",
-        // id: "price_1RIsBwEJQRIZgEwebyRvlBN8",
-        id: "price_1RQ8P3EJQRIZgEweJhQTQK43",
-        name: "14 Dias De Anúncio",
-        description: "Seu anúncio ficará visível por 14 dias",
-        price: "€95.00"
-    },
-    {
-        // id: "price_1RAt3DCuEJW1dWBajo4sXGXG",
-        // id: "price_1RIsBuEJQRIZgEweQPe3PrRW",
-        id: "price_1RQ8PYEJQRIZgEwe5Fqd4EwJ",
-        name: "30 Dias De Anúncio",
-        description: "Seu anúncio ficará visível por 30 dias",
-        price: "€140.00"
-    }
+  {
+    // id: "price_1RAt1OCuEJW1dWBav2YzW54D",
+    // id: "price_1RIsByEJQRIZgEwenkVNKNPt",
+    // id: "price_1RQ8O9EJQRIZgEweScjlRZY1",
+    id: 'price_1RQA2eEJQRIZgEwex35RbMMP',
+    name: '7 Dias De Anúncio',
+    description: 'Seu perfil vai ser visivel no topo da sua cidade por 7 dias',
+    price: '€30.00',
+  },
+  {
+    // id: "price_1RAt2nCuEJW1dWBajoTLZQF1",
+    // id: "price_1RIsBwEJQRIZgEwebyRvlBN8",
+    // id: 'price_1RQ8P3EJQRIZgEweJhQTQK43',
+    id: 'price_1RQA37EJQRIZgEwee89HkBet',
+    name: '14 Dias De Anúncio',
+    description: 'Seu anúncio ficará visível por 14 dias',
+    price: '€40.00',
+  },
+  {
+    // id: "price_1RAt3DCuEJW1dWBajo4sXGXG",
+    // id: "price_1RIsBuEJQRIZgEweQPe3PrRW",
+    id: 'price_1RQA3iEJQRIZgEwe0vUw7ehc',
+    name: '30 Dias De Anúncio',
+    description: 'Seu anúncio ficará visível por 30 dias',
+    price: '€60.00',
+  },
 ];
 
 export default async function CheckoutPage() {
+  const { userId } = await auth();
+  const hasPaid = await hasCompanionPaid(userId as string);
+  if (hasPaid) {
+    redirect('/profile');
+  }
 
-    const { userId } = await auth();
-    const hasPaid = await hasCompanionPaid(userId as string);
-    if (hasPaid) {
-        redirect("/profile");
-    }
-
-    return (
-        <div className="container mx-auto py-10">
-            <h1 className="text-2xl font-bold mb-6">Selecione duração do Anúncio</h1>
-            <p className="mb-4">
-                Escolha a duração do seu anúncio. Após o pagamento, seu perfil será visível no topo da sua cidade, atraindo mais clientes.
-            </p>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                {products.map((product) => (
-                    <ProductCard key={product.id} product={product} />
-                ))}
-            </div>
-        </div>
-    );
-};;
+  return (
+    <div className="container mx-auto py-10">
+      <h1 className="text-2xl font-bold mb-6">Selecione duração do Anúncio</h1>
+      <p className="mb-4">
+        Escolha a duração do seu anúncio. Após o pagamento, seu perfil será
+        visível no topo da sua cidade, atraindo mais clientes.
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+}
--- components/whatsapp-button.tsx
+++ components/whatsapp-button.tsx
@@ -1,30 +1,32 @@
-"use client";
+'use client';
 
-import { IconBrandWhatsapp } from "@tabler/icons-react";
-import Link from "next/link";
+import { IconBrandWhatsapp } from '@tabler/icons-react';
+import Link from 'next/link';
 
 interface WhatsAppButtonProps {
-    phoneNumber?: string;
-    message?: string;
+  phoneNumber?: string;
+  message?: string;
 }
 
 export const WhatsAppButton = ({
-    phoneNumber = "351912979481",
-    message = "",
+  phoneNumber = '351914156606',
+  message = '',
 }: WhatsAppButtonProps) => {
-    const whatsappUrl = `https://wa.me/${phoneNumber}${message ? `?text=${encodeURIComponent(message)}` : ""}`;
+  const whatsappUrl = `https://wa.me/${phoneNumber}${
+    message ? `?text=${encodeURIComponent(message)}` : ''
+  }`;
 
-    return (
-        <Link
-            href={whatsappUrl}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="fixed bottom-6 right-6 bg-green-500 hover:bg-green-600 text-white rounded-full p-4 shadow-lg transition-all duration-300 ease-in-out z-50 flex items-center justify-center"
-        >
-            <IconBrandWhatsapp />
-            <span className="ml-2">Contato</span>
-        </Link>
-    );
+  return (
+    <Link
+      href={whatsappUrl}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="fixed bottom-6 right-6 bg-green-500 hover:bg-green-600 text-white rounded-full p-4 shadow-lg transition-all duration-300 ease-in-out z-50 flex items-center justify-center"
+    >
+      <IconBrandWhatsapp />
+      <span className="ml-2">Contato</span>
+    </Link>
+  );
 };
 
 export default WhatsAppButton;
