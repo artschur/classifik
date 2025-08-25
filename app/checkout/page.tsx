5aa7461f1834984696e7efa2e137ffa59a65f5f0
Arthur Schurhaus <arthurschur@icloud.com>
2025-08-25 15:28:04 -0300

    updated checkout page  for promotion text

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,7 +1,12 @@
 import { auth } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
 import { ProductCard } from './productCard';
-import { BASIC_PRICE_ID, hasActiveAd, PLUS_PRICE_ID, VIP_PRICE_ID } from '@/db/queries/kv';
+import {
+  BASIC_PRICE_ID,
+  hasActiveAd,
+  PLUS_PRICE_ID,
+  VIP_PRICE_ID,
+} from '@/db/queries/kv';
 import { isVerificationPending } from '../actions/document-verification';
 export interface Product {
   id: string;
@@ -15,7 +20,8 @@ const products: Product[] = [
   {
     id: BASIC_PRICE_ID, // Replace with your new recurring price ID
     name: 'Básico',
-    description: 'Presença estratégica com recursos essenciais para se destacar. Assinatura mensal',
+    description:
+      'Presença estratégica com recursos essenciais para se destacar. Assinatura mensal',
     benefits: [
       'Destaque na listagem da cidade escolhida',
       'Exibição antes dos anúncios gratuitos',
@@ -27,7 +33,8 @@ const products: Product[] = [
   {
     id: PLUS_PRICE_ID, // Replace with your new recurring price ID
     name: 'Plus',
-    description: 'Mais visibilidade e prioridade para o seu perfil. Assinatura mensal.',
+    description:
+      'Mais visibilidade e prioridade para o seu perfil. Assinatura mensal.',
     benefits: [
       'Posição de maior destaque na cidade escolhida',
       'Prioridade nas buscas (acima de anúncios Básico e Gratuito)',
@@ -42,7 +49,8 @@ const products: Product[] = [
   {
     id: VIP_PRICE_ID, // Replace with your new recurring price ID
     name: 'VIP',
-    description: 'Máximo destaque e prioridade total para o seu perfil. Assinatura mensal.',
+    description:
+      'Máximo destaque e prioridade total para o seu perfil. Assinatura mensal.',
     benefits: [
       'Destaque absoluto na sua cidade',
       'Prioridade máxima nos resultados de busca',
@@ -66,7 +74,10 @@ export default async function CheckoutPage() {
     redirect('/sign-in');
   }
 
-  const [isUserVerified, hasPaid] = await Promise.all([isVerificationPending(userId), hasActiveAd(userId as string)]);
+  const [isUserVerified, hasPaid] = await Promise.all([
+    isVerificationPending(userId),
+    hasActiveAd(userId as string),
+  ]);
 
   if (isUserVerified) {
     redirect('/companions/verification/pending');
@@ -80,8 +91,16 @@ export default async function CheckoutPage() {
     <div className="container mx-auto py-10">
       <h1 className="text-2xl font-bold mb-6">Selecione seu Anúncio</h1>
       <p className="mb-4">
-        Escolha o seu anúncio. Após o pagamento, seu perfil será visível no topo da sua cidade, atraindo mais clientes.
+        Escolha o seu anúncio. Após o pagamento, seu perfil será visível no topo
+        da sua cidade, atraindo mais clientes.
       </p>
+      <div className="flex flex-col items-start justify-start my-6 p-6 border border-neutral-800 rounded-lg bg-card">
+        <h2 className="text-neutral-100 text-xl">
+          Na escolha de um plano, os 2 primeiros meses são grátis (apenas para
+          os 15 primeiros clientes)
+        </h2>
+        <p className="text-lg text-neutral-400">Cancele quando quiser!</p>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
