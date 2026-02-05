00d68066d28ded541b6a2d9520dafa275dcf0dce
Arthur Schurhaus <arthurschur@icloud.com>
2026-02-05 15:29:44 -0300

    updated proxy to ignore images folder

--- proxy.ts
+++ proxy.ts
@@ -58,7 +58,7 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
   matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
     "/(api|trpc)(.*)",
   ],
 };
