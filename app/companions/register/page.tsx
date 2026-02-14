a66b62dfdce84233936f6364dcd93fcdf8ae5cdb
Arthur Schurhaus <arthurschur@icloud.com>
2026-02-13 22:30:02 -0300

    serialize better

--- app/companions/register/page.tsx
+++ app/companions/register/page.tsx
@@ -80,8 +80,8 @@ async function CompanionFormWithData() {
 
   return (
     <RegisterCompanionForm
-      cities={JSON.parse(JSON.stringify(cities))}
-      companionData={companion ? JSON.parse(JSON.stringify(companion)) : null}
+      cities={cities.map(city => Object.assign({}, city))}
+      companionData={companion ? Object.assign({}, companion) : null}
     />
   );
 }
