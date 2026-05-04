0c7db5eb4f6759522aa359a6b50aa2e966e7e8a4
brunenzo01 <brunoenzo.andrade@gmail.com>
2026-05-04 16:11:58 -0300

    seo: add GA4, native robots.ts and fix schema logo

--- app/robots.ts
+++ app/robots.ts
@@ -2,27 +2,25 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: [
-        '/',
-        '/location/',
-        '/companions/',
-        '/blog/',
-        '/*?utm_',
-      ],
-      disallow: [
-        '/checkout',
-        '/onboarding',
-        '/login',
-        '/register',
-        '/admin',
-        '/api/',
-        '/profile/',
-        '/*?s=',
-        '/*?search=',
-      ],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: ['/', '/location/', '/companions/', '/blog/'],
+        disallow: [
+          '/_next/static/',
+          '/_next/image',
+          '/checkout',
+          '/onboarding',
+          '/login',
+          '/register',
+          '/admin',
+          '/api/',
+          '/profile/',
+          '/*?s=',
+          '/*?search=',
+        ],
+      },
+    ],
     sitemap: 'https://www.onesugar.pt/sitemap.xml',
   };
 }
